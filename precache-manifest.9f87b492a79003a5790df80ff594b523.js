710dbe10cab7329d681c6c77d18788d49d9c60f2
Helder.Souza <helder.souza@benner.com.br>
2018-11-12 19:10:29 -0200

    Updates

---
+++ precache-manifest.9f87b492a79003a5790df80ff594b523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-dashboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "748513f2188d10425871",
+    "url": "/react-dashboard/static/js/runtime~main.748513f2.js"
+  },
+  {
+    "revision": "2cbd723060298b9fc539",
+    "url": "/react-dashboard/static/js/main.2cbd7230.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/react-dashboard/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "2cbd723060298b9fc539",
+    "url": "/react-dashboard/static/css/main.ce564811.chunk.css"
+  },
+  {
+    "revision": "c956e3832b40cb651d5a10c1b0ae3abc",
+    "url": "/react-dashboard/index.html"
+  }
+];
